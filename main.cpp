d326b2d271c1d638ae8d224cfec991c6d4bca399
Silvertrousers <c.goodarzi@gmail.com>
2022-01-01 22:46:27 +0000

    main

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+
+
+class inventory {
+    std::vector<Item> Items;
+    
+};
+
+class Player {
+    Inventory inventory;
+
+};
+
+class StoryNode {
+    public:
+        std::vector<StoryNode*> Decisions;
+        std::string Text;
+        location
+};
+
+//need to be able to add story lines
+//how to construct a story line
+//ideally the functionality allows me to add a story line or quest, maybe have a strcutre of quests
+
+//need to be able to construct the story graph from a set of nodes, charcters
+int main(){
+    StoryNode
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
+
+basically it will look like
